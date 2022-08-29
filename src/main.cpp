6e5c7ea75bb2d8f1c4d8bb4abeaa938cd8fe40da
Erik Nylander <nylander.e@gmail.com>
2022-08-29 23:51:04 +0200

    Parse some things

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,23 @@
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
 #include "CRC16.h"
+#include <cstdlib>
 
-const char *ssid = "Dannfelt@elva";
-const char *password = "Nyland3r@home1";
+const char *ssid = "---------";
+const char *password = "---------";
 
+#define MAXLINELENGTH 128 // longest normal line is 47 char (+3 for \r\n\0)
+char telegram[MAXLINELENGTH];
+
+double mTotal = 0.0;
+double mActive = 0.0;
 
 void sendHTTP(String data)
 {
   WiFiClient client;
   HTTPClient http;
 
-  String serverPath = "http://192.168.68.68:3000/echo";
+  String serverPath = "http://192.168.0.3:3000/echo";
 
   // Your Domain name with URL path or IP address with path
   http.begin(client, serverPath.c_str());
@@ -61,32 +67,72 @@ void setup()
   sendHTTP("ESP8266 connected");
 }
 
-void readTelegram() {
-  if (Serial.available()) {
-    char message[128];
-    int count = 0;
-    while (Serial.available()) {
-      char buf[1];
-      Serial.readBytes(buf, 1);
-      message[count] = buf[0];
-      yield();
-      count++;
-      // if(decodeTelegram(len+1))
-      // {
-      //    UpdateElectricity();
-      //    UpdateGas();
-      // }
+int indexOfChar(char *raw, char target)
+{
+  for (int i = 0; i < strlen(raw); i++)
+  {
+    if (raw[i] == target)
+    {
+      return i;
     }
-    message[count] = 0;
-    // Serial.printf("Read count: %d\n", count);
-    sendHTTP(String(count));
-    sendHTTP(String(message));
-    // Serial.println(String(message));
+  }
+  return -1;
+}
+
+double parseDouble(char *raw)
+{
+  int start = indexOfChar(raw, '(');
+  int end = indexOfChar(raw, '*');
+
+  int len = end - start;
+
+  char rawValue[len + 1];
+  memcpy(rawValue, &raw[start + 1], len - 1);
+  rawValue[end - start - 1] = '\0';
+
+  return std::atof(rawValue);
+}
+
+bool decodeTelegram(char *raw)
+{
+  if (raw[0] == '!')
+    return true;
+
+  if (strncmp(raw, "1-0:1.8.0", strlen("1-0:1.8.0")) == 0)
+    mTotal = parseDouble(raw);
+
+  if (strncmp(raw, "1-0:1.7.0", strlen("1-0:1.7.0")) == 0)
+    mActive = parseDouble(raw);
+
+  return false;
+}
+
+void readTelegram()
+{
+  if (Serial.available())
+  {
+    memset(telegram, 0, sizeof(telegram));
+    while (Serial.available())
+    {
+      int len = Serial.readBytesUntil('\n', telegram, MAXLINELENGTH);
+      telegram[len] = '\n';
+      telegram[len + 1] = 0;
+      yield();
+
+      char raw[len + 2];
+      strcpy(raw, telegram);
+
+      if (decodeTelegram(raw))
+      {
+        Serial.println("Decoded message");
+        Serial.printf("mTotal: %f\n", mTotal);
+        Serial.printf("mActive: %f\n", mActive);
+      }
+    }
   }
 }
 
-
-
-void loop() {
+void loop()
+{
   readTelegram();
 }
